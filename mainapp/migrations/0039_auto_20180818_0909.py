271c03215fc94ca54b1b8f99d55ba0e4aae91f2e
ananthuma <ananthakrishnanma@gmail.com>
2018-08-18 10:01:15 +0530

    Changed migration dependancy
    Changed migration dependancy 0039_auto_20180818_0328

--- mainapp/migrations/0039_auto_20180818_0909.py
+++ mainapp/migrations/0039_auto_20180818_0909.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('mainapp', '0038_auto_20180817_2159'),
+        ('mainapp', '0039_auto_20180818_0328'),
     ]
 
     operations = [
